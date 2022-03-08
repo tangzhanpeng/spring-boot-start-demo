042c8ebd543e3514bd7351d225d4d185690f5f7b
zhanpeng.tang <807324130@qq.com>
2022-03-08 10:04:56 +0800

    fix

---
+++ atguigu-hello-spring-boot-starter-autoconfigure/src/main/java/com/atguigu/atguiguhellospringbootstarterautoconfigure/AtguiguHelloSpringBootStarterAutoconfigureApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.atguiguhellospringbootstarterautoconfigure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtguiguHelloSpringBootStarterAutoconfigureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtguiguHelloSpringBootStarterAutoconfigureApplication.class, args);
+    }
+
+}
---
+++ atguigu-hello-spring-boot-starter-autoconfigure/src/test/java/com/atguigu/atguiguhellospringbootstarterautoconfigure/AtguiguHelloSpringBootStarterAutoconfigureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.atguiguhellospringbootstarterautoconfigure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtguiguHelloSpringBootStarterAutoconfigureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
